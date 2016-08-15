0e3c469943c9e5ead9a11c5ead3238d8e076d931
Michaela Chevalier <michaelajc@gmail.com>
2016-08-15 12:23:03 -0700

    A9: forEach & cleanup

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
+ var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
 var animatePoints = function(points) {
-    
-    var revealPoint = function(i) {
-    
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-               
-        };
-    
-    for (var i=0; i < points.length; i++) {  
-        revealPoint(i);
-        }   
-    };
+    forEach(points, revealPoint);
+};
 
 window.onload = function() {
+    var points = document.getElementsByClassName('point');
     
     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+         animatePoints(points);
+    }
     
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);   
+        }
     });
 }
